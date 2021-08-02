5d1dd1c7c6e9308d7f1bcbbe6c30253089878f47
Ubuntu <ruser@microtest.qsacwvat5fpurel3u0ewjpxoyh.ix.internal.cloudapp.net>
2021-08-02 11:08:57 +0000

    initial

---
+++ src/main/java/in/humIndia/store/DDD2/Ddd2Application.java
@@ -0,0 +1,15 @@
+package in.humIndia.store.DDD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ddd2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Ddd2Application.class, args);
+		Sytem.out.println("hello");
+	}
+
+}
